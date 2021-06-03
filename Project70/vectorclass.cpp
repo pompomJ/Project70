30f1c969e00d009b08b93e6305ae89a76f78caba
ZamaVerce2 <Verce@LAPTOP-8JD0V3Q5>
2021-06-03 13:05:50 +0900

    プロジェクト ファイルを追加します。

---
+++ Project70/vectorclass.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> v1;
+
+	v1.push_back(10);
+	v1.push_back(11);
+	v1.push_back(12);
+
+	cout << "—v‘f‚Ì”:" << (int)v1.size() << endl;
+	cout << "v1[0] = " << v1[0] << endl;
+	cout << "v1[1] = " << v1[1] << endl;
+	cout << "v1[2] = " << v1[2] << endl;
+
+	return 0;
+}
